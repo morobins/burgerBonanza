610111e53905ce312b136a32b3b70a10680d2b9f
Monique Robinson <monique.robinson2@gmail.com>
2018-05-02 18:28:13 -0400

    Fixed file casing

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,29 @@
 //bring in mysql
 var mysql = require("mysql");
 
-// if (process.env.JAWSDB-URL) {
-//   connection.mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'hacktheplanet',
-//     database: 'todoagain_db'
-//   });
-// };
+if (process.env.JAWSDB-URL) {
+  connection.mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  });
+};
 
 //connect to localhost
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
 
-  // Your username
-  user: "root",
+//   // Your username
+//   user: "root",
 
-  // Your password
-  password: "",
-  database: "burgers_db"
-});
+//   // Your password
+//   password: "",
+//   database: "burgers_db"
+// });
 
 connection.connect(function (err) {
   if (err) throw err;
