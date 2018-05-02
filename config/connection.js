e6e2d69e3059108e470451c73f1586608d3f72a7
Monique Robinson <monique.robinson2@gmail.com>
2018-05-01 20:19:19 -0400

    File Setup

---
+++ config/connection.js
@@ -0,0 +1,35 @@
+//bring in mysql
+var mysql = require("mysql");
+
+if (process.env.JAWSDB-URL) {
+  connection.mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhose',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  });
+};
+
+//connect to localhost
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "burgers_db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  // console.log("Connected as id " + connection.threadId);
+  callFunction();
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO 
+  burgers (burger_name, devoured)
+VALUES 
+  ("6 Alarm Burger", false), 
+  ("Mighty Mushroom Burger", false), 
+  ("Big Bacon Burger", false);
